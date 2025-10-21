ab92f3086e1b39591d5e4e40d8bb1f1f3ae20c53
aura <joshuaura7822@gmail.com>
2025-10-21 04:56:11 +0300

    Refactor EDA.py for new data source and plots
    Updated data loading and visualization for EDA.

--- EDA.py
+++ EDA.py
@@ -5,21 +5,19 @@
 #==============================
 #CODE FOR EPLARATORY DATA ANALYSIS
 
-import panda as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import pandas as pd #data manipulation and statistical summary
+import seaborn as sns # advanced aesthetic plots
+import matplotlib.pyplot as plt # basic plotting
 
-df = pd.read_csv('data.csv')
+aura = pd.read_csv('/home/aura/Documents/2.2/datascience/data.csv') #reads command separated values from path in pc
+print(aura.shape) # Responsible for dimensions ,rows and colums
+print(aura.info()) #prints data types and checks for missing values
+print(aura.describe()) #shows  satistical summaries sof numerical colums
 
-print(df.shape)
-print(df.info())
-print(df.descibe())
-
-sns.heatmap(df.isnull(),char=False)
+sns.heatmap(aura.isnull(),cbar = True)
 plt.show()
 
-df.hist(figsize=(10,0))
+aura.hist(figsize=(10,0))
 plt.show()
-
-sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+sns.heatmap(aura.corr(numeric_only=True), annot=True, cmap='coolwarm')
 plt.show()
